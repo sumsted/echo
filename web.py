d332d6b32be477eb3e83eb1d1ed4487716b3275f
Scott Umsted <scottumsted@gmail.com>
2015-10-10 22:15:28 -0500

    fix port

--- web.py
+++ web.py
@@ -30,7 +30,7 @@ def post_message():
     return {'messages': messages}
 
 
-if os.environ.get('APP_LOCATION') == 'heroku':
+if os.environ.get('ON_HEROKU'):
     run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
     run(host='localhost', port=8080, debug=True)
